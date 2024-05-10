2481e72f42b635d584ceeceda65af05b1d9a6275
Henok <hennok100@gmai.com>
2024-05-09 20:17:55 -0400

    favicon

--- app/book/page.tsx
+++ app/book/page.tsx
@@ -7,7 +7,7 @@ import Navbar from "@/components/Navbar";
 const Book = () => {
   return (
     <>
-      <div className="flex flex-col   w-full  h-screen bg-black/[0.96]  bg-grid-white/[0.02]   ">
+      <div className="flex flex-col   w-full  h-screen bg-[#001f33]/[0.96]  bg-grid-white/[0.02]   ">
         <Navbar
           scrollToWebsiteDesign={() => {}}
           scrollToGraphicDesign={() => {}}
@@ -16,7 +16,7 @@ const Book = () => {
         />
         <div
           className="text-4xl pb-5 md:text-6xl text-center 
-        bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50"
+        bg-clip-text text-transparent bg-gradient-to-b from-[#deb992] to-neutral-400 bg-opacity-50"
         >
           Book a meeting
         </div>
